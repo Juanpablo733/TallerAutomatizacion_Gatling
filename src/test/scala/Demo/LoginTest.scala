1544bc6af354cb00d12ed33893366fe11c52676a
Juan Pablo Bedoya <76190539+Juanpablo733@users.noreply.github.com>
2025-06-28 14:34:07 -0500

    Update LoginTest.scala

--- src/test/scala/Demo/LoginTest.scala
+++ src/test/scala/Demo/LoginTest.scala
@@ -22,6 +22,14 @@ class LoginTest extends Simulation{
       .check(jsonPath("$.token").saveAs("authToken"))
     )
 
+      .exec(
+      http("Create Contact")
+        .post(s"contacts")
+        .header("Authorization", "Bearer ${authToken}")
+        .body(StringBody(s"""{"firstName": "Andres","lastName": "Gomez","birthdate": "1970-01-01","email": "jdoe@fake.com","phone": "8005555555","street1": "1 Main St.","street2": "Apartment A","city": "Anytown","stateProvince": "KS","postalCode": "12345","country": "USA"}""")).asJson
+        .check(status.is(201))
+    )
+
   // 3 Load Scenario
   setUp(
     scn.inject(rampUsersPerSec(5).to(15).during(30))
